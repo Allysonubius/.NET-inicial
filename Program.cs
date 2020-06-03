cd45c500bf4ad4457913b15e2c733be3eedab2fd
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-06-02 21:48:29 -0300

    Finalizado video aula sobre C# e estrutura de programa.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 
-namespace _NET_inicial {
-    class Program {
-        static void Main (string[] args) {
-            //Inteiro array (numero de vezes repetidos) = 5
-            int numeroDeVezes = 5;
-            //sendo o inteiro i o valor de 0, ele sera contado o numero de vezes menor que i sendo 0,1,2,3,4.
-            for (int i = 0; i < numeroDeVezes; i++)
-            //Estamos pedindo para ele mostrar o texto com o valor que fizemos acima.
-                Console.WriteLine ($"Bem vindo ao mundo .NET {i}");
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var s = new Pilha();
+            s.Empilha("Cliente " + 1);
+            s.Empilha("Cliente " + 2);
+            s.Empilha("Cliente " + 3);
+
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
         }
     }
 }
